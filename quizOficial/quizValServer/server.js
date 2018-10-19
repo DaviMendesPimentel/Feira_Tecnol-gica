9af4e0b8cc564d0d85b0ee5968d3a88fed5fd499
eclonit <41867983+eclonit@users.noreply.github.com>
2018-10-19 12:28:37 -0300

    Update server.js

--- quizOficial/quizValServer/server.js
+++ quizOficial/quizValServer/server.js
@@ -1,5 +1,5 @@
-var iterador = 0;
-var respostas = [0, 2, 3, 0, 2];
+var iterador = 0;						// variável responsável por armazenar o número da questão
+var respostas = [0, 2, 3, 0, 2];				// cria um vetor responsável por armazenar as respostas das questões
 var express = require('express'); 				// inclui a biblioteca 'express'
 var appExpress = express(); 					// linka 'appExpress' com o contrutor 'express()'
 var path = require("path"); 					// inclui a biblioteca 'path'
@@ -35,60 +35,68 @@ appExpress.get('/', function(req, res){ 			// o servidor executa como referênci
 
 appExpress.use('/libraries', express.static('node_modules')); 	//manda o servidor utilizar as bibliotecas da pasta node_modules
 
-function validacao(escolha) {
-        console.log("validacao chamada.");
-	if(respostas[iterador] == escolha){
-                var intervalo = setInterval(function(){
-                        if(LEDVERDE.readSync() == 0)
-                                LEDVERDE.writeSync(1);
-                        else LEDVERDE.writeSync(0);
-                }, 150);
-                setTimeout(function(){ clearInterval(intervalo); LEDVERDE.writeSync(0); }, 1000);
+// função responsável por validar a escolha com base no vetor de 'respostas' criado a cima
+function validacao(escolha) {	// recebe 'escolha' como argumento
+        console.log("validacao chamada.");	// console.log() chamada por questões de debug
+	if(respostas[iterador] == escolha){	// se o jogador acerto:
+                var intervalo = setInterval(function(){	// cria um intervalor
+                        if(LEDVERDE.readSync() == 0)	// se a led Verde estiver desligada
+                                LEDVERDE.writeSync(1);	// liga a led Verde
+                        else LEDVERDE.writeSync(0);	// se não, desliga ela
+                }, 150);	// executa essa função a cada 0,15 segundos
+		
+		// executa 'clearInterval(interval)' após 1,0 segundo
+		// 'clearInterval()' é uma função própria do javascript que quebra um intervalo, no caso o nosso 'intervalo'
+		// e ainda desliga a led Verde
+                setTimeout(function(){ clearInterval(intervalo); LEDVERDE.writeSync(0); }, 1000); // 1000 == 1 segundo
 
-		if(iterador >= 4){
-                	iterador = 0;
-        	}else{
-                	++iterador;
+		if(iterador >= respostas.length){	// se o iterador for maior ou igual que o tamanho do vetor 'respostas'
+                	iterador = 0;	// reseta o iterador
+        	}else{	// se não:
+                	++iterador;	// incrementa o iterador
        		 }
-	        console.log("valor de iterador: " + iterador);
+	        console.log("valor de iterador: " + iterador);	// mostra o valor de 'iterador' por questões de debug
 
-		return true;
-	}else{
-                var intervalo = setInterval(function(){
-                        if(LEDVERMELHA.readSync() == 0)
-                                LEDVERMELHA.writeSync(1);
-                        else LEDVERMELHA.writeSync(0);
-                }, 250);
+		return true;	// retorna true, pois o jogador acertou a pergunta
+	}else{	// se não
+                var intervalo = setInterval(function(){	// cria um intervalo
+                        if(LEDVERMELHA.readSync() == 0)	// se a led Vermelha estiver desligada
+                                LEDVERMELHA.writeSync(1); // liga ela
+                        else LEDVERMELHA.writeSync(0);	// se não, desliga ela
+                }, 250); // executa isso a cada 0,25 segundos 
+		
+		// quebra o intervalor criado após 1,0 segundo
                 setTimeout(function(){ clearInterval(intervalo); LEDVERMELHA.writeSync(0); }, 1000);
 
-		if(iterador >= 4){
-                	iterador = 0;
-        	}else{
-               		 ++iterador;
+		if(iterador >= respostas.length){	// se o iterador for maior ou igual ao tamanho do vetor 'respostas'
+                	iterador = 0;	// reseta o iterador
+        	}else{	// se não:
+               		 ++iterador;	// incrementa o 'iterador'
        		 }
-	        console.log("valor de iterador: " + iterador);
+	        console.log("valor de iterador: " + iterador); // mostra o valor de 'iterador' por questões de debug
 
-		return false;
+		return false;	// retorna false, pois o jogador errou a pergunta
         }
-  }
+  }	// fim de 'validacao()'
 
-io.sockets.on('connection', function (socket) {			// cria uma conecção websocket utilizando o namespace 'connection'
+io.sockets.on('connection', function (socket) {		// cria uma conecção websocket com o client utilizando o namespace 'connection'
 
-	socket.on('assistirBotoes', function(num_daQuestao){
-
-	 BOTAO0.watch(function (err, value) {
-	console.log("Botao0.watch...");
-        if(err){
-                console.log("erro ocorreu no botao0: " + err);
-                throw err;
-        }
-        if(value != 0){
-		console.log("Chamando validacao...");
-                var result = validacao(0, num_daQuestao);
-                io.emit('message', result);
-                }
-        });
-
+   socket.on('assistirBotoes', function(num_daQuestao){	// função ouvinte do evento 'assistirBotoes', recebe o número da questão
+							// como argumento
+	BOTAO0.watch(function (err, value) {	// assiste ao pino do 'BOTAO0'
+		console.log("Botao0.watch...");	// 'console.log()' chamado por questões de debug
+        	if(err){	// se a função devolver um erro:
+                	console.log("erro ocorreu no botao0: " + err);	// mostra no console.
+                	throw err;	// e dispara o erro para finalizar o programa
+        	}
+        	if(value != 0){	// se o valor do pino do 'BOTAO0' se tornar diferente de 0:
+			console.log("Chamando validacao...");	// 'console.log()' chamado por questões de debug
+                	var result = validacao(0, num_daQuestao); // 'result' recebe o valor de 'return' de 'validacao()'
+                	io.emit('message', result);	// emit 'result' para todos os ouvintes 'message'(temos apenas 1 ouvinte
+							// 'message')
+                	}
+        	});
+	// execulta o mesmo algoritmo feito no 'BOTAO0' porém, com 1 ao invés do zero
  	BOTAO1.watch(function (err, value) {
         if(err){
                 console.log("erro ocorreu no botao1: " + err);
@@ -99,7 +107,7 @@ io.sockets.on('connection', function (socket) {			// cria uma conecção websock
                 io.emit('message', result);
                 }
         });
-
+	// execulta o mesmo algoritmo feito no 'BOTAO0' porém, com 2 ao invés do zero
  	BOTAO2.watch(function (err, value) {
 	console.log("Botao2.watch...");
         if(err){
@@ -112,6 +120,7 @@ io.sockets.on('connection', function (socket) {			// cria uma conecção websock
                 io.emit('message', result);
                 }
         });
+	// execulta o mesmo algoritmo feito no 'BOTAO0' porém, com 3 ao invés do zero
     	BOTAO3.watch(function (err, value) {
         if(err){
                 console.log("erro ocorreu no botao3: " + err);
